024383d72a45a3dbf5ec73aca7f256ac4985aac5
moi1496 <MOISES@DESKTOP-2TA44ML>
2022-06-26 21:01:07 -0500

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.rjProgressBar1 = new CustomControls.RJControls.RJProgressBar();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 50000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // rjProgressBar1
+            // 
+            this.rjProgressBar1.ChannelColor = System.Drawing.Color.Transparent;
+            this.rjProgressBar1.ChannelHeight = 5;
+            this.rjProgressBar1.ForeBackColor = System.Drawing.Color.Transparent;
+            this.rjProgressBar1.ForeColor = System.Drawing.Color.Transparent;
+            this.rjProgressBar1.Location = new System.Drawing.Point(3, 75);
+            this.rjProgressBar1.Name = "rjProgressBar1";
+            this.rjProgressBar1.ShowMaximun = false;
+            this.rjProgressBar1.ShowValue = CustomControls.RJControls.TextPosition.Right;
+            this.rjProgressBar1.Size = new System.Drawing.Size(1280, 61);
+            this.rjProgressBar1.SliderColor = System.Drawing.Color.Gold;
+            this.rjProgressBar1.SliderHeight = 4;
+            this.rjProgressBar1.SymbolAfter = "";
+            this.rjProgressBar1.SymbolBefore = "";
+            this.rjProgressBar1.TabIndex = 0;
+            this.rjProgressBar1.Value = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 700);
+            this.Controls.Add(this.rjProgressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CustomControls.RJControls.RJProgressBar rjProgressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
